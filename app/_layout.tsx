9aeaad62154432534d967535b32ac89714383843
“markj82” <markjohn666@gmail.com>
2025-02-23 13:59:42 +0000

    setup nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,8 @@ import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
 import { useEffect } from 'react'
 import 'react-native-reanimated'
+import 'react-native-get-random-values'
+import './global.css'
 
 export {
   // Catch any errors thrown by the Layout component.
